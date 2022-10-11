02efb6cdd37bb918ea54ee365ff84c1be1093e7a
nitin-cha <105273624+nitin-cha@users.noreply.github.com>
2022-10-11 16:29:26 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@
 
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!made some changes by me "); 
+        System.out.println("Hello, World!made some changes by me and done smoothly "); 
     }
 }
